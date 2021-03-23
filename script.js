8449b02bd5e095f06548aceb72f94812c3121fba
abutler6588 <abutler6588@gmail.com>
2021-03-23 14:52:47 -0500

    updated display to include humidity, windspeed, and location name

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
 var button = document.querySelector('.button')
 var inputValue = document.querySelector('.inputValue')
-var name = document.querySelector('.name');
+var city = document.querySelector('.city');
 var desc = document.querySelector('.desc');
 var temp = document.querySelector('.temp');
+var humidity = document.querySelector('.humidity');
+var wind = document.querySelector('.wind');
+
+// button.addEventListener('click',function(){
+//     fetch('https://api.openweathermap.org/data/2.5/weather?zip='+inputValue.value+'&units=imperial&appid=9a255556c2668bb58d6390fe1aa8f993')
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+
+//     .catch(err => alert("Wrong city zip code!"))
+// })
 
 button.addEventListener('click',function(){
     fetch('https://api.openweathermap.org/data/2.5/weather?zip='+inputValue.value+'&units=imperial&appid=9a255556c2668bb58d6390fe1aa8f993')
     .then(response => response.json())
     .then(data => {
-        var nameValue = data['name'];
-        var tempValue = data['main']['temp'];
+        var cityValue = data['name'];
+        var tempValue = data['main']['humidity'];
         var descValue = data['weather'][0]['description'];
-        
-        name.innerHTML = nameValue;
-        temp.innerHTML = tempValue + ' °F';
+        var windValue = data['wind']['speed'];
+        var humidityValue = data['main']['humidity'];
+
+        city.innerHTML = 'Weather in ' + cityValue;
+        temp.innerHTML = tempValue + '°F';
         desc.innerHTML = descValue;
+        humidity.innerHTML = 'Humidity: ' + humidityValue + '%';
+        wind.innerHTML = 'Wind: ' + windValue + 'mp/h';
     })
 
-    
 
     .catch(err => alert("Wrong city zip code!"))
 })
